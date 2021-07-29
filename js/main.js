f36adf07e89695eda8c6747839e5059169b21fc4
Pablo Levin <pablevin@gmail.com>
2021-07-28 23:28:34 -0300

    DOM

--- js/main.js
+++ js/main.js
@@ -1,67 +1,131 @@
-
 //MANEJO Y VENTA DE CALZADOS: Se calcula monto a pagar y se actualiza el stock de la tienda.
 //CLASE CONSTRUCTORA Calzado CREADORA DE OBJETOS (los llamaremos calzadoX)
 class Product{
-    constructor (id, category, gender, brand, model, color, size, price, quantity){
+    constructor (id, category, gender, brand, model, color, price, sizeStock){
         this.id = id;
         this.category = category.toUpperCase(); //Posibilidad de vender otro producto que no sean Productos y escalar a una especie de mercadolibre.
         this.gender = gender.toUpperCase(); //Se va a usar en un futuro para filtrar (Hombre, Mujer, Niño).
         this.brand = brand.toUpperCase();
         this.model = model.toUpperCase();
         this.color = color.toUpperCase();
-        this.size = parseInt(size);
         this.price = parseFloat(price);
-        this.quantity = parseInt(quantity);
-        this.img1 = "img/" + this.gender.toLowerCase() + "/" + this.brand.toLowerCase() + "/" + this.id + "_" + this.brand.toLowerCase() + "_" + this.model.toLowerCase() + "_" + this.color.toLowerCase() + "_a.webp"; //Crea URL de imagen 1
-        this.img2 = "img/" + this.gender.toLowerCase() + "/" + this.brand.toLowerCase() + "/" + this.id + "_" + this.brand.toLowerCase() + "_" + this.model.toLowerCase() + "_" + this.color.toLowerCase() + "_b.webp"; //Crea URL de imagen 2
-        this.stock = true;
+        this.sizeStock = sizeStock;
+        this.img1 = 'img/' + this.gender.toLowerCase() + '/' + this.brand.toLowerCase() + '/' + this.id + '_' + this.brand.toLowerCase() + '_' + this.model.toLowerCase() + '_' + this.color.toLowerCase() + '_a.webp'; //Crea URL de imagen 1
+        this.img2 = 'img/' + this.gender.toLowerCase() + '/' + this.brand.toLowerCase() + '/' + this.id + '_' + this.brand.toLowerCase() + '_' + this.model.toLowerCase() + '_' + this.color.toLowerCase() + '_b.webp'; //Crea URL de imagen 2
+        this.inStock = true;
         this.promotion = false;
     }
     fixStock() { //MODIFICO PROPIEDADES CANTIDAD Y STOCK DEL OBJETO
-        this.quantity = this.quantity - 1; //RESTO LA COMPRA
-        
-        if(this.quantity <= 0){ //CHEQUEO STOCK
-            this.stock = false;
-        }
+        // this.stock = this.stock - 1; //RESTO LA COMPRA
+        // if(this.stock <= 0){ //CHEQUEO STOCK
+        //     this.inStock = false;
+        // }
+        // console.log(this.sizeStock[0].size)
+        // products.forEach( product => {
+        //     let array = product.stock;
+        //     array.forEach ((array) => {
+        //         suma += array;
+        //     });
+        //     console.log(suma);  
+        // });   
+
+        //TENGO QUE ARREGLAR ESTO CON FIND, PARA QUE DESCUENTE SOLO DEL TALLE QUE SE COMPRO. DETECTO CUAL FUE CON FIND Y LE HAGO LA RESTA POR INDEX DEL ARRAY
+        products.forEach( product => { 
+            product.sizeStock.forEach(element => { // RECORRO EL ARRAY sizeStock
+                element.stock = element.stock -1; //AGREGO EL VALOR AL BOTÓN
+            });
+        });
+        products.forEach( product => {
+            
+            let sum = 0;
+            product.sizeStock.forEach(element => { // RECORRO EL ARRAY sizeStock
+
+                sum += element.stock; //AGREGO EL VALOR AL BOTÓN
+                
+            });
+            if(sum <= 0){ //CHEQUEO STOCK
+                product.inStock = false;
+            }
+            // console.log(product.id+ "= "+ sum);  
+        });
     }
 }
+
 //END CLASE CONSTRUCTORA Producto
 
 //BASE DE DATOS SIMULADA en array de objetos productos.
 const products = []; //Creo el array;
-products.push(new Product(1, "calzado", "hombre",  "Topper", "classic", "black", 40, 1800, 3));
-products.push(new Product(2, "calzado", "hombre", "Topper", "rainbow", "multi", 39, 1500, 10));
-products.push(new Product(3, "calzado", "mujer", "Topper", "cebra", "black_red", 41, 2800, 31));
-products.push(new Product(4, "calzado", "mujer", "Topper", "lover", "light-grey", 38, 3100, 5));
-products.push(new Product(5, "calzado", "hombre", "Adidas", "infinity", "black", 40, 5100, 1));
-products.push(new Product(6, "calzado", "hombre", "Adidas", "js", "black", 41, 3100, 5));
-products.push(new Product(7, "calzado", "hombre", "Adidas", "angular", "black", 38, 2100, 10));
-products.push(new Product(8, "calzado", "hombre", "Adidas", "rougue", "black", 39, 4200, 1));
-
+products.push(new Product(1, 'calzado', 'hombres', 'Topper', 'classic', 'black', 1800, [{'size': 34, 'stock': 2},{'size': 35, 'stock': 0},{'size':36, 'stock':1}]));
+products.push(new Product(2, 'calzado', 'hombres', 'Topper', 'rainbow', 'multi', 1500, [{'size': 37, 'stock': 4},{'size': 40, 'stock': 5},{'size':42, 'stock':1}]));
+products.push(new Product(3, 'calzado', 'hombres', 'Topper', 'cebra', 'black_red', 2800, [{'size': 37, 'stock': 30},{'size': 3 , 'stock': 335},{'size':42, 'stock':0}]));
+products.push(new Product(4, 'calzado', 'hombres', 'Topper', 'lover', 'light-grey', 3100, [{'size': 10, 'stock': 3}]));
+products.push(new Product(5, 'calzado', 'hombres', 'Adidas', 'infinity', 'black', 5100, [{'size': 15, 'stock': 0}]));
+products.push(new Product(6, 'calzado', 'hombres', 'Adidas', 'js', 'black', 3100, [{'size': 39, 'stock': 0},{'size': 40, 'stock': 5},{'size':41, 'stock':1}]));
+products.push(new Product(7, 'calzado', 'hombres', 'Adidas', 'angular', 'black', 2100, [{'size': 35, 'stock': 3},{'size': 40, 'stock': 5}]));
+products.push(new Product(8, 'calzado', 'hombres', 'Adidas', 'rougue', 'black', 4200, [{'size': 16, 'stock': 9}]));
+console.log(products);
+// products[0].fixStock();
 
 console.log(products);
-console.log("------SCRIPT INICIALIZADO------- AHORA PUEDE ORDENAR MEDIANTE SORT O COMPRAR MEDIANTE BUY");
+console.log('------SCRIPT INICIALIZADO------- AHORA PUEDE ORDENAR MEDIANTE SORT O COMPRAR MEDIANTE BUY');
 
+// CARGANDO EL DOM con FRAGMENT y el TEMPLATE DE HTML
+const productsCatalog = document.getElementById('productsCatalog');
+const templateProduct = document.getElementById('templateProduct').content;
+const fragment = document.createDocumentFragment();
+products.forEach( product => {
+    templateProduct.querySelector('.img-a').src= product.img1; //img/hombres/topper/1_topper_classic_black_a.webp
+    templateProduct.querySelector('.img-b').src= product.img2;
+    templateProduct.querySelector('.img-brand-card').src='img/brands/' + product.brand + '.svg';
+    templateProduct.querySelector('.card-title').textContent = product.model;
+    templateProduct.querySelector('.price').textContent = '$'+product.price;
+    templateProduct.querySelector('.btn-group').innerHTML = ''; //LIMPIO PARA NO REPOBLAR
+    
+    product.sizeStock.forEach(element => { // RECORRO EL ARRAY sizeStock
+        const addBtns = templateProduct.querySelector('.btn-group');
+        let button=document.createElement('button'); //CREO EL BOTÓN DE TALLES
+        button.classList.add('btn', 'btn-outline-primary', 'm-1');
+        button.textContent = element.size; //AGREGO EL VALOR AL BOTÓN
+        if (element.stock === 0){
+            button.classList.add('btn-outline-secondary');
+            button.disabled = true;
+            addBtns.appendChild(button);
+        }else{
+            addBtns.appendChild(button);
+        }
+    });
+
+    const clone = templateProduct.cloneNode(true);
+    fragment.appendChild(clone);
+});
+productsCatalog.appendChild(fragment);
+// END TEMPLATE
+
+let cart = document.querySelectorAll('.btn-group .btn');
+cart.forEach(element => {element.addEventListener('click', function() {
+    let pressedBtn=parseInt(element.textContent);
+    alert(pressedBtn);
+})});
+// products[1].fixStock()
 //****FILTROS****
 //ORDENAR POR PRECIO
 function sortDescending(){ //DESCENDENTE
-    console.log("PRECIO MÁS ALTO PRIMERO");
+    console.log('PRECIO MÁS ALTO PRIMERO');
     products.sort((a, b) => b.price - a.price)
 };
 
 function sortAscending(){ //ASCENDENTE
-    console.log("PRECIO MÁS BAJO PRIMERO");
+    console.log('PRECIO MÁS BAJO PRIMERO');
     products.sort((a, b) => a.price - b.price)
 };
 
 //FILTRAR POR RANGO DE PRECIOS
 let priceRanges = [];
 function priceRange(){ 
-    let lowRange = parseInt(prompt("Ingrese su precio mínimo"));
-    let highRange = parseInt(prompt("Ingrese su precio máximo"));
-    console.log("NEW PRICE RANGE");
+    let lowRange = parseInt(prompt('Ingrese su precio mínimo'));
+    let highRange = parseInt(prompt('Ingrese su precio máximo'));
+    console.log('NEW PRICE RANGE');
     priceRanges = products.filter( a => a.price > lowRange && a.price < highRange);
-    
 };
 //****END FILTROS****
 function buy(){
@@ -83,15 +147,15 @@ function buy(){
                     userEntry = idSelected;
                     break;
                 case 2:
-                    buyQuantity = parseInt(prompt("Nuestro stock para: " + buyObject.brand + " - " +buyObject.model + " es de: " + buyObject.quantity + "\n¿Cuantos pares lleva?"));
-                    userEntry = buyQuantity;
+                    buyStock = parseInt(prompt('Nuestro stock para: ' + buyObject.brand + ' - ' +buyObject.model + ' es de: ' + buyObject.stock + '\n¿Cuantos pares lleva?'));
+                    userEntry = buyStock;
                     break;
                 case 3:
-                    saleCupon = prompt("No podemos otorgar ese descuento. El descuento mayor es de 50% ¿Cuál es el % de desuento de su cupón?");
+                    saleCupon = prompt('No podemos otorgar ese descuento. El descuento mayor es de 50% ¿Cuál es el % de desuento de su cupón?');
                     userEntry = saleCupon;
                     break;
                 case 4:
-                    dues = prompt("Debe pagar $" + Math.round(payWithSale) +". Ingrese la cantidad de dues. El máximo de dues es 10");
+                    dues = prompt('Debe pagar $' + Math.round(payWithSale) +'. Ingrese la cantidad de cuotas. El máximo de cuotas es 10');
                     userEntry = dues;
                     break;
                 default:
@@ -115,27 +179,27 @@ function buy(){
 
     validation(idSelected, 8, 1); //Llamo a la funcion para validar los datos del usuario
 
-    // let buyObject = eval("calzado" + idSelected); //SELECCION DINAMICA (Selecciono objeto seleccionado por usuario)
+    // let buyObject = eval('calzado' + idSelected); //SELECCION DINAMICA (Selecciono objeto seleccionado por usuario)
     let buyObject = products[idSelected-1]; //SELECCION DINAMICA (Selecciono objeto seleccionado por usuario)
     // console.log(buyObject);
-    let buyQuantity = parseInt(prompt("¿Cuántos pares lleva?"));
-    validation(buyQuantity, buyObject.quantity, 2); //Llamo a la funcion para validar los datos del usuario
+    let buyStock = parseInt(prompt('¿Cuántos pares lleva?'));
+    validation(buyStock, buyObject.stock, 2); //Llamo a la funcion para validar los datos del usuario
     //END USUARIO
 
     //CALCULO (Se hacen diferentes cálculos)
     //FUNCION VENTA (calculo precio con iva teniendo en cuenta la cantidad de compra, bajo el stock la cantidad de compra, admito venta si la compra es menor o igual al stock y si además hay stock)
     function sold(){ 
-        for (var i = 0; i < buyQuantity; i++) { //Arregla el stock (le resta lo que compró) *****ESTO SE DEBERÍA HACER DESPUES DE CHEQUEAR QUE EL USUARIO PAGÓ*****
+        for (var i = 0; i < buyStock; i++) { //Arregla el stock (le resta lo que compró) *****ESTO SE DEBERÍA HACER DESPUES DE CHEQUEAR QUE EL USUARIO PAGÓ*****
             buyObject.fixStock(); 
         }
         
-        let montoTotal = buyObject.price * buyQuantity * 1.22; 
+        let montoTotal = buyObject.price * buyStock * 1.22; 
         return montoTotal;
     };
     let totalNoCupon = sold();
     //END CALCULO
     //USUARIO DESCUENTO
-    let saleCupon = prompt("Otorgamos descuentos de hasta el 50% de su valor de compra. ¿Cuál es el % de descuento de su cupón?");
+    let saleCupon = prompt('Otorgamos descuentos de hasta el 50% de su valor de compra. ¿Cuál es el % de descuento de su cupón?');
     if (saleCupon != 0){
         validation(saleCupon, 50, 3); //Llamo a la funcion para validar los datos del usuario
     }
@@ -148,8 +212,8 @@ function buy(){
         //DESCUENTO
         saleCupon = (100 - parseInt(saleCupon))/100;
         payWithSale = totalNoCupon * saleCupon;
-        //USUARIO dues
-        dues = prompt("Debe pagar $" + Math.round(payWithSale) +". Ingrese la cantidad de dues. El máximo de dues es 10");
+        //USUARIO CUOTAS
+        dues = prompt('Debe pagar $' + Math.round(payWithSale) +'. Ingrese la cantidad de dues. El máximo de dues es 10');
         validation(dues, 10, 4); //Llamo a la funcion para validar los datos del usuario
         let totalPay, duesPay;
         //END USUARIO
@@ -160,29 +224,29 @@ function buy(){
     let aPagar = pago(); // Respuesta de funcion pago. Devuelve un array.
     //END CALCULO
     //INFO PARA EL USUARIO
-    alert("USTED ESTÁ COMPRANDO: \nCalzado: " + buyObject.brand + " - " + buyObject.model + "\nCantidad: " + buyQuantity  + "\n-------------\nSu descuento es del: " + Math.round((100-(saleCupon*100))) + "%\n-------------\nDebe pagar: $" + Math.round(aPagar[0]) + "\ndues: " + aPagar[1] + "\nPago Mensual: $" + Math.round(aPagar[2]));
+    alert('USTED ESTÁ COMPRANDO: \nCalzado: ' + buyObject.brand + ' - ' + buyObject.model + '\nCantidad: ' + buyStock  + '\n-------------\nSu descuento es del: ' + Math.round((100-(saleCupon*100))) + '%\n-------------\nDebe pagar: $' + Math.round(aPagar[0]) + '\ndues: ' + aPagar[1] + '\nPago Mensual: $' + Math.round(aPagar[2]));
     //Muestra como quedo el objeto modificado luego de la compra por consola ya que este valor es relevante solo para la tienda.
     console.log(buyObject);
     console.log(products);
 }
 //***SORT****
-document.getElementById("sortDescending").addEventListener("click", function() {
+document.getElementById('sortDescending').addEventListener('click', function() {
     sortDescending();
     console.log(products);
 });
-document.getElementById("sortAscending").addEventListener("click", function() {
+document.getElementById('sortAscending').addEventListener('click', function() {
     sortAscending();
     console.log(products);
 });
 //****END SORT****
 //****RANGE****
-document.getElementById("priceRange").addEventListener("click", function() {
+document.getElementById('priceRange').addEventListener('click', function() {
     priceRange();
     console.log(priceRanges);
 });
 //****END RANGE****
 //****BOTON BUY****
-document.getElementById("buy").addEventListener("click", function() {
+document.getElementById('buy').addEventListener('click', function() {
     buy();
 });
 //****END BOTON BUY****
