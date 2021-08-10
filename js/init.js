6cb416a82a1f37b7882c71a0fc569cbe89e4e900
Pablo Levin <pablevin@gmail.com>
2021-08-10 13:18:19 -0300

    This is a new commit for what I originally planned to be amended

--- js/init.js
+++ js/init.js
@@ -2,3 +2,29 @@
 [...document.querySelectorAll('[data-bs-toggle="tooltip"]')].forEach(el => new bootstrap.Tooltip(el));
 // ENABLE popover
 [...document.querySelectorAll('[data-bs-toggle="popover"]')].forEach(el => new bootstrap.Popover(el));
+
+
+
+
+
+// let el = document.getElementById('buy');
+// let toggleClass = document.getElementById('sidebar');
+// el.addEventListener("click", toggle())
+// function toggle() {
+//     toggleClass.classList.toggle('closeSidebar');
+// }
+
+let closeSidebar = document.getElementById('closeSidebar');
+let cartIcon = document.getElementById('cartIcon');
+let toggleClass = document.getElementById('sidebar');
+let toggleClass2 = document.getElementById('sidebarOverlay');
+
+cartIcon.addEventListener('click', toggleSidebar);
+closeSidebar.addEventListener('click', toggleSidebar);
+toggleClass2.addEventListener('click', toggleSidebar);
+function toggleSidebar(e) {
+    e.preventDefault();
+    toggleClass.classList.toggle('sidebar-off');
+    toggleClass2.classList.toggle('overlay-off');
+    document.body.classList.toggle('no-scroll');
+};
